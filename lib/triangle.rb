cd6c22ee287df12a1443b9bfdd83ccdc376447b0
Amal Abdu <abduamal@ymail.com>
2020-05-06 04:07:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,12 @@
 class Triangle
-  
+  attr_accessor :x, :y, :z
+
+  def initialize(x,y,z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
   class TriangleError < StandardError
     # triangle error code
   end
